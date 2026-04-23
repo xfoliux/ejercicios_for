f70216b51c97d29349159c49f39259c5c6b741cd
xfoliux <cristiangualan91@gmail.com>
2026-04-23 00:56:50 -0500

    Se crea archivo videos.txt para pegar el link del video de la explicacion del debug de la function listarImpares

--- ejercicios.js
+++ ejercicios.js
@@ -11,6 +11,8 @@ function ejecutar(numEjercicio){
         listarNumeroReversa();
     } else if(numEjercicio == 3){
         listarPares();
+    } else if(numEjercicio == 4){
+        listarImpares();
     }
 }
 
@@ -21,6 +23,11 @@ function listarNumeroReversa(){
 }
 function listarPares(){
     for( let i=0; i<10; i+=2){
+        console.log(i);
+    }
+}
+function listarImpares(){
+    for(let i=1; i<=7; i+=2){
         console.log(i);
     }
 }
