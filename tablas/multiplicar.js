97f0b99a8f78139aacadc01ceef3ccd777ebe2a1
xfoliux <cristiangualan91@gmail.com>
2026-04-24 00:31:18 -0500

    se realizar cambios en html y se crea funciones para multiplicar cualquier numero deseado

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,10 +1,22 @@
 function generarTablas(){
+    let numero = document.getElementById("txtNumber").value;
     let contenedor = document.getElementById("contenedorTabla");
     contenedor.innerHTML = " <h1>PROBANDO</h1>";
     let contenido = "";
 
-    for( i = 0 ; i <= 10 ; i++){
-        contenido = contenido + "<p>" + "5 x " + i + " = " + (5*i) + "</p>";
+    if(numero < 0 ){
+        alert("Tiene que ser numero mayor a 0");
+        return;
+    }else if(numero == 0 ){
+        alert("tiene que ser mayor a cero");
+        return;
+    }else if(numero % 1 ){
+        alert("tiene que ser numeros enteros")
+        return;
+    }
+
+    for( i = 1 ; i <= 10 ; i++){
+        contenido = contenido + "<p>" + numero + "x" +  i + " = " + (numero * i) + "</p>";
     }
     contenedor.innerHTML = contenido;
 
